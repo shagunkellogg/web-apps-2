5fea65d0b9466e1b224e5cdcd1cf73d96169f656
Shagun Sharma <shagun.sharma@kellogg.northwestern.edu>
2025-03-06 06:10:31 +0000

    in-class exercise, some things might not work in the new contacts thing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,17 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
-  #   # start with a new Post
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Post
+    @posts = Post.new
+    # assign user-entered form data to Post's columns
+    @posts["author"] = params["author"]
+    @posts["body"] = params["body"]
+    @posts["image"] = params["image"]
+    # save Post row
+    @posts.save
+    # redirect user
+    redirect_to "/posts"
+  end
 
 end
